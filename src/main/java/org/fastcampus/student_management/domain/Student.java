2216f544a84e5145091e5148cdf7e72c9ff10c0b
gangsungu <ark_@naver.com>
2025-03-04 23:38:59 +0900

    과제 완료 2025-03-04

--- src/main/java/org/fastcampus/student_management/domain/Student.java
+++ src/main/java/org/fastcampus/student_management/domain/Student.java
@@ -18,6 +18,24 @@ public class Student {
     this.activated = true;
   }
 
+  // setter는 객체지향에서 캡슐화를 떨어뜨림
+  // 레이어별로 정보를 전달하기 위한 dto 객체를 제외하면 setter 사용은 지양하는게 좋음
+  public void activate() {
+    if(this.activated) {
+      throw new IllegalArgumentException();
+    }
+
+    this.activated = true;
+  }
+
+  public void deactivate() {
+    if(!this.activated) {
+      throw new IllegalArgumentException();
+    }
+
+    this.activated = false;
+  }
+
   public String getName() {
     return name;
   }
